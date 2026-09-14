4a5cb5860216843d4213a5217a81f27139ca28fd
Joshua Jones <105621089+jsquaresec@users.noreply.github.com>
2026-09-14 14:44:55 -0500

    Add legacy command support to client

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ const {
 } = require('discord.js');
 
 const loadCommands = require('./handlers/commandHandler');
+const loadLegacyCommands = require('./handlers/legacyCommandHandler');
 const loadEvents = require('./handlers/eventHandler');
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
 loadCommands(client);
+loadLegacyCommands(client);
 loadEvents(client);
 
 client.login(process.env.DISCORD_TOKEN);
